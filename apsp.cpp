f84d5ef1c13a7bb4433a23905341e96cc787c225
quinnhj <JohnsonQ531@gmail.com>
2014-04-30 02:52:37 -0700

    Added code to use the bucket heaps. It appears to work, but currently doing a slow-as-hell remove() instead of a constant time remove. Working on that now....
    Also, to avoid edge cases, we're enforcing a minimum size on edge weights to avoid an overflow bucket situation.

--- apsp.cpp
+++ apsp.cpp
@@ -8,9 +8,13 @@
 #include <boost/heap/binomial_heap.hpp>
 
 #define HEAP fibonacci_heap
+#define BUCKET
 
 using namespace boost::heap;
 const float INF = 100000.0;
+float min_edge = 1.0;
+int last_bucket = 0;
+bool empty = false;
 //Note, reversed comparator, so call "increase" when decreasing key.
 HEAP<vert_pair, compare<vert_comparator> > heap;
 
@@ -40,8 +44,57 @@ void floyd_warshall(int n, int* par, float* dist) {
 
 }
 
+void heap_insert(std::list<int>* bucket_heap, int* b_num, int pair, int val, int n,
+        std::list<int>::iterator* iters) {
+    int idx = min((int)(val/min_edge), n*n);
+    bucket_heap[idx].push_back(pair);
+    b_num[pair] = idx;
+    if (bucket_heap[idx].size() < 1) {
+        printf("SIZE OF ZERO\n");
+    }
+    std::list<int>::iterator iter = bucket_heap[idx].end();
+    iter--;
+    iters[pair] = iter;
+}
+
+
+void heap_decrease(std::list<int>* bucket_heap, int* b_num, int pair, int val, int n,
+        std::list<int>::iterator* iters) {
+    int orig_idx = b_num[pair];
+    std::list<int> list = bucket_heap[orig_idx];
+    
+    //Naive O(N) removal of item from linked list. Slow as hell.
+    list.remove(pair);
+    //What we should do, but it's not that simple :/
+    //list.erase(iters[pair]);
+    heap_insert(bucket_heap, b_num, pair, val, n, iters);
+}
+
+
+int heap_extract(std::list<int>* bucket_heap, int* b_num, int n,
+        std::list<int>::iterator* iters) {
+    int ret_val;
+    for (int i = last_bucket; i < n*n + 1; i++,last_bucket++) {
+        if (bucket_heap[i].size() > 0) {
+            if (i < n*n) {
+                ret_val = bucket_heap[i].front();
+                bucket_heap[i].pop_front();
+                return ret_val;
+            } else {
+                //This should be implemented using a real heap as per the paper.
+                // For now enforcing that this scenario can't happen by limiting
+                // the minimum edge value.
+                return -1;
+            }
+        }
+    }
+    return -1;
+}
+
 void di_init(int n, float* dist, float* graph, vert_pair* q_arr, int* p, int* q,
              std::vector<int>* L, std::vector<int>* R,
+             std::list<int>* bucket_heap, int* b_num,
+             std::list<int>::iterator* iters,
              HEAP<vert_pair, compare<vert_comparator> >::handle_type* handles) {
     
     for (int i = 0; i < n; i++) {
@@ -74,7 +127,12 @@ void di_init(int n, float* dist, float* graph, vert_pair* q_arr, int* p, int* q,
                 vert.u = i;
                 vert.v = j;
                 vert.dist = graph[i*n + j];
+                
+                #ifdef BUCKET
+                heap_insert(bucket_heap, b_num, i*n + j, graph[i*n + j], n, iters);
+                #else
                 handles[i*n + j] = heap.push(vert);
+                #endif
             }
         }
     }
@@ -82,6 +140,8 @@ void di_init(int n, float* dist, float* graph, vert_pair* q_arr, int* p, int* q,
 
 void di_examine(int n, int u, int v, int w, float* dist, float* graph, vert_pair* q_arr, 
                 int* p, int* q, std::vector<int>* L, std::vector<int>* R,
+                std::list<int>* bucket_heap, int* b_num,
+                std::list<int>::iterator* iters,
                 HEAP<vert_pair, compare<vert_comparator> >::handle_type* handles) {
     if (dist[u*n + v] + dist[v*n + w] < dist[u*n + w]) {
         dist[u*n + w] = dist[u*n + v] + dist[v*n + w];
@@ -92,9 +152,17 @@ void di_examine(int n, int u, int v, int w, float* dist, float* graph, vert_pair
         vert.dist = dist[u*n + w];
             
         if (p[u*n + w] == -1) {    
+            #ifdef BUCKET
+            heap_insert(bucket_heap, b_num, u*n + w, dist[u*n + w], n, iters);
+            #else
             handles[u*n + w] = heap.push(vert);
+            #endif
         } else {
+            #ifdef BUCKET
+            heap_decrease(bucket_heap, b_num, u*n + w, dist[u*n + w], n, iters);
+            #else
             heap.increase(handles[u*n + w], vert);
+            #endif
         }
 
         p[u*n + w] = p[u*n + v];
@@ -105,6 +173,8 @@ void di_examine(int n, int u, int v, int w, float* dist, float* graph, vert_pair
 
 void di_lists(int n, int u, int v, float* dist, float* graph, vert_pair* q_arr, 
                 int* p, int* q, std::vector<int>* L, std::vector<int>* R,
+                std::list<int>* bucket_heap, int* b_num,
+                std::list<int>::iterator* iters,
                 HEAP<vert_pair, compare<vert_comparator> >::handle_type* handles) {
 
     int lidx = u*n + q[u*n + v];
@@ -114,36 +184,51 @@ void di_lists(int n, int u, int v, float* dist, float* graph, vert_pair* q_arr,
     for (i = 0; i != L[lidx].size(); i++) {
 
         di_examine(n, L[lidx][i], u, v, dist, graph, q_arr, 
-            p, q, L, R, handles);
+            p, q, L, R, bucket_heap, b_num, iters, handles);
 
     }
   
     for (i = 0; i != R[ridx].size(); i++) {
 
         di_examine(n, u, v, R[ridx][i], dist, graph, q_arr, 
-            p, q, L, R, handles);
+            p, q, L, R, bucket_heap, b_num, iters, handles);
 
     }
 }
 
 void di_apsp(int n, float* dist, float* graph, vert_pair* q_arr, 
              int* p, int* q, std::vector<int>* L, std::vector<int>* R,
+             std::list<int>* bucket_heap, int* b_num,
+             std::list<int>::iterator* iters,
              HEAP<vert_pair, compare<vert_comparator> >::handle_type* handles) {
     
-    di_init(n, dist, graph, q_arr, p, q, L, R, handles);
-    int u, v;
+    di_init(n, dist, graph, q_arr, p, q, L, R, bucket_heap, b_num, iters, handles);
+    int u, v, pair;
     //Main loop
+    #ifdef BUCKET
+    while (1) {
+    #else
     while (!heap.empty()) {
+    #endif
+
+        #ifdef BUCKET
+        pair = heap_extract(bucket_heap, b_num, n, iters);
+        if (pair < 0) break;
+        u = pair/n;
+        v = pair%n;
+        #else
         vert_pair vert = heap.top();
         u = vert.u;
         v = vert.v;
         //Has to happen AFTER setting u and v, or there will be a segfault
         heap.pop();
+        #endif
+        
         L[p[u*n + v]*n + v].push_back(u);
         R[u*n + q[u*n + v]].push_back(v);
 
         di_lists(n, u, v, dist, graph, q_arr, 
-            p, q, L, R, handles);
+            p, q, L, R, bucket_heap, b_num, iters, handles);
     }
 }
 
@@ -178,12 +263,17 @@ int main( int argc, char **argv )
     
     // Initialize every edge to have a weight between 0 and 1.
     srand48(1337);
+    float min_allowable_value = (1.0/(n*n));
     for (int i = 0; i < n * n; i++) {
+        float temp;
         //no edge between an edge and itself.
         if (i%n == i/n) {
             graph[i] = INF;
         } else {
-            graph[i] = (float)drand48();
+            temp = (float)drand48();
+            if (temp < min_allowable_value) temp += min_allowable_value;
+            if (temp < min_edge) min_edge = temp;
+            graph[i] = temp;
         }
     }
     //Print for viewing if small enough
@@ -214,11 +304,15 @@ int main( int argc, char **argv )
     std::vector<int> *R = new std::vector<int>[n*n];
     HEAP<vert_pair, compare<vert_comparator> >::handle_type *handles = 
             new HEAP<vert_pair, compare<vert_comparator> >::handle_type[n*n];
+    std::list<int> *bucket_heap = new std::list<int>[n*n + 1];
+    std::list<int>::iterator *iters = new std::list<int>::iterator[n*n];
+    int *b_num = (int*) malloc(n * n * sizeof(int));
 
     //Initialize arrays
     for (int i = 0; i < n*n; i++) {
         fw_dist[i] = graph[i];
         di_dist[i] = graph[i];
+        b_num[i] = 0;
     }
 
     //
@@ -227,7 +321,7 @@ int main( int argc, char **argv )
     
     di_time = read_timer();
     di_apsp(n, di_dist, graph, q_arr, 
-            p, q, L, R, handles);
+            p, q, L, R, bucket_heap, b_num, iters, handles);
     di_time = read_timer() - di_time;
 
     //Running Floyd Warshall for standard.
@@ -305,10 +399,7 @@ int main( int argc, char **argv )
     free(q_arr);
     free(p);
     free(q);
-    
-    //Not necessary with new?
-    //free(L);
-    //free(R);
+    free(b_num);
     
     return 0;
 }
